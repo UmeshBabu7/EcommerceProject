15b130cf791d9b65873b50a766ede6251f52257a
umeshbabu7 <umeshtamang129@gmail.com>
2024-02-08 14:32:09 +0545

    add media/admins

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -113,6 +113,12 @@ STATIC_ROOT = BASE_DIR / "static_cdn"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "umeshtamang129@gmail.com"
+EMAIL_HOST_PASSWORD ="xjdh pghv ghmz vrxi"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
